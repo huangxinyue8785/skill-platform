a8380fdf21e8dc7d56197c7ff4b6587402aa89d1
huangxinyue8785 <hxy0375@outlook.com>
2026-04-09 12:36:29 +0800

    重新添加 db.js 和 cos.js（环境变量版本）

---
+++ server/config/cos.js
@@ -0,0 +1,9 @@
+// config/cos.js
+const COS = require('cos-nodejs-sdk-v5');
+
+const cos = new COS({
+    SecretId: process.env.COS_SECRET_ID,
+    SecretKey: process.env.COS_SECRET_KEY,
+});
+
+module.exports = cos;
